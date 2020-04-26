3849ce1d6c6a3e9d38ed7540a319fac3906fcb5a
Thanh Bui <congthanh184@gmail.com>
2020-04-26 22:15:24 +0700

    Add drive_bot subscribe to cmd_vel

--- ball_chaser/src/drive_bot.cpp
+++ ball_chaser/src/drive_bot.cpp
@@ -1,11 +1,24 @@
 #include <std_msgs/Float64.h>
+#include <geometry_msgs/Twist.h>
 #include "ball_chaser/DriveToTarget.h"
 #include "ros/ros.h"
 
+ros::Publisher botCmd;
+
 bool handle_command(ball_chaser::DriveToTarget::Request& req,
                     ball_chaser::DriveToTarget::Response& res) {
-  ROS_INFO("DriveToTarget request received - linear_x:%1.2f, angular_z:%1.2f",
-           (float)req.linear_x, (float)req.angular_z);
+  // ROS_INFO("DriveToTarget request received - linear_x:%1.2f, angular_z:%1.2f",
+  //          (float)req.linear_x, (float)req.angular_z);
+  geometry_msgs::Twist cmd;
+  cmd.linear.x = (float)req.linear_x;
+  cmd.linear.y = 0.0;
+  cmd.linear.z = 0.0;
+
+  cmd.angular.x = 0.0;
+  cmd.angular.y = 0.0;
+  cmd.angular.z = (float)req.angular_z;
+
+  botCmd.publish(cmd);
   return true;
 }
 
@@ -16,6 +29,7 @@ int main(int argc, char** argv) {
   ros::ServiceServer service =
       n.advertiseService("/ball_chaser/command_robot", handle_command);
 
+  botCmd = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
   ROS_INFO("Ready to send robot commands");
 
   ros::spin();
--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -3,29 +3,58 @@
 #include "ball_chaser/DriveToTarget.h"
 #include "ros/ros.h"
 
+enum class BotState { REST, ROTATING, RUNNING };
+
 class SubscribeAndCallService {
  public:
+  static const uint16_t LEFT = 1000;
+  static const uint16_t RIGHT = 1600;
+  static const uint32_t STOP_THRESHOLD = 150000;
+
   SubscribeAndCallService() {
     client_ = n_.serviceClient<ball_chaser::DriveToTarget>(
         "/ball_chaser/command_robot");
     sub_ = n_.subscribe("/camera/rgb/image_raw", 10,
                         &SubscribeAndCallService::imgCallback, this);
 
-    pub_ = n_.advertise<sensor_msgs::Image>("/ball_chaser/processed_image", 10);
-    // ball_chaser::DriveToTarget srv;
-    // srv.request.angular_z = 0.5;
-    // for (int i = 0; i < 5; i++) {
-    //   ros::Duration(5).sleep();
-    //   srv.request.linear_x = i;
+    state_ = BotState::REST;
+    ros::Duration(0.5).sleep();
+    brake();
+  }
 
-    //   if (!client_.call(srv)) {
-    //     ROS_INFO("Error calling service");
-    //   }
-    // }
+  void callDriveToTarget(const double linear_x, const double angular_z) {
+    ball_chaser::DriveToTarget srv;
+    srv.request.angular_z = angular_z;
+    srv.request.linear_x = linear_x;
+
+    if (!client_.call(srv)) {
+      ROS_INFO("Error calling service");
+    }
+  }
+
+  void rotateLeft() {
+    ROS_INFO("GO LEFT");
+    callDriveToTarget(0.0, 0.2);
+  }
+
+  void rotateRight() {
+    ROS_INFO("GO RIGHT");
+    callDriveToTarget(0.0, -0.2);
+  }
+
+  void goStraight() {
+    ROS_INFO("GO STRAIGHT");
+    callDriveToTarget(0.2, 0.0);
+  }
+
+  void brake() {
+    ROS_INFO("BRAKE");
+    callDriveToTarget(0.0, 0.0);
   }
 
   void imgCallback(const sensor_msgs::Image img) {
     uint32_t sumCol = 0, sumRow = 0, area = 0;
+    uint32_t centerCol;
 
     for (uint32_t i = 0; i < img.height * img.step; i++) {
       if (img.data[i] == 255) {
@@ -35,9 +64,21 @@ class SubscribeAndCallService {
       }
     }
 
-    if (area > 0) {
+    if (area > 0 && area < STOP_THRESHOLD) {
+      centerCol = sumCol / area;
+
       ROS_INFO("row_center: %d, col_center: %d, area: %d", sumRow / area,
-               sumCol / area, area);
+               centerCol, area);
+
+      if (centerCol < LEFT) {
+        rotateLeft();
+      } else if (centerCol > RIGHT) {
+        rotateRight();
+      } else {
+        goStraight();
+      }
+    } else {
+      brake();
     }
   }
 
@@ -46,6 +87,7 @@ class SubscribeAndCallService {
   ros::Subscriber sub_;
   ros::Publisher pub_;
   ros::ServiceClient client_;
+  BotState state_;
 };
 
 int main(int argc, char* argv[]) {
